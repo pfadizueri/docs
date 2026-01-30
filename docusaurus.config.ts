d6971288bfd3b43651ad94732efbb1251bff72ee
battino46 <battino46@gmail.com>
2026-01-30 12:52:48 +0000

    replace invalid links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
           label: 'Über uns',
         },
         {
-          href: 'https://github.com/pfadizueri/website',
+          href: 'https://github.com/pfadizueri/docs',
           label: 'GitHub',
           position: 'right',
         },
@@ -105,7 +105,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/pfadizueri/website',
+              href: 'https://github.com/pfadizueri/docs',
             },
           ],
         },
