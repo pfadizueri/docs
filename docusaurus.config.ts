a45a79fdd0a5e49258586262eed4fbd463dfa2d4
Laurin Berli <mail@laurinberli.ch>
2026-02-03 19:09:19 +0000

    Entferne Impressum und Datenschutz Links aus dem Copyright-Hinweis

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -126,7 +126,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `© ${new Date().getFullYear()} Pfadi Züri | <a href="https://pfadi.zuerich/impressum/" target="_blank">Impressum</a> | <a href="https://pfadi.zuerich/datenschutz/" target="_blank">Datenschutz</a> | Built with Docusaurus.`,
+      copyright: `© ${new Date().getFullYear()} Pfadi Züri | Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
